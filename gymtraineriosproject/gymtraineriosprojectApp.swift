62526b4c6cb837ae915d252649a60362c060285f
Mila <kehayova.mila@gmail/com>
2023-10-09 19:31:46 -0400

    Initial Commit

---
+++ gymtraineriosproject/gymtraineriosprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymtraineriosprojectApp.swift
+//  gymtraineriosproject
+//
+//  Created by user249179 on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct gymtraineriosprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
